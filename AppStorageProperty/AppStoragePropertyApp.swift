271676b564b802cf4935d790a056b44285467280
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-07-01 13:48:20 +0300

    Initial Commit

---
+++ AppStorageProperty/AppStoragePropertyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoragePropertyApp.swift
+//  AppStorageProperty
+//
+//  Created by ramil on 01.07.2020.
+//
+
+import SwiftUI
+
+@main
+struct AppStoragePropertyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
